27dead0edcabc9b85f886cbc90f01db7413bd775
Hugo Arnoult <hugo.arnoult83@gmail.com>
2020-08-11 14:31:17 +0200

    Add Create new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # -- Read All tasks --
   get 'tasks' => 'tasks#index'
+  # -- Create a new task --
+  get 'tasks/new' => 'tasks#new', as: :new_task
+  post 'tasks' => 'tasks#create'
+  # -- Read One task --
   get 'tasks/:id' => 'tasks#show', as: :task
 end
